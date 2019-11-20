8cc484284c75344d78b816193e1f839ab114a5f0
Gonzalo Ramirez <goramirez@H6450.ad.hugeinc.com>
2019-11-20 16:01:26 -0500

    post base styles, post sections

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,32 @@ export default {
 </script>
 
 <style>
+:root {
+  --color-gray: #2c3e50;
+  --color-dark-gray: #2d2d2d;
+  --color-darkest-gray: #333;
+  --color-white: #fff;
+}
+
 html {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--color-gray);
+}
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+.wrapper {
+  display: flex;
+  flex-flow: row;
+}
+.left-block {
+  width: 30%;
+}
+.right-block {
+  width: 70%;
 }
 </style>
--- src/components/Init.vue
+++ src/components/Init.vue
@@ -2,7 +2,31 @@
   <div class="container">
     <div class="wrapper">
       <div class="left-block">
-        hola
+        <div class="posts-block">
+          <div class="posts-block--header">
+            <h4>Reddit Posts</h4>
+          </div>
+          <div class="posts-block--content" :key="post.title" v-for="post in posts">
+
+            <div class="post">
+              <div class="unseen"></div>
+              <div class="post-author">
+                <h5>By: {{post.author}}</h5> <h6>{{post.entryDate()}}</h6>
+              </div>
+              <div class="post-content">
+                <img v-bind:src="post.thumbnail" v-if="post.thumbnail.substring(0,4) === 'http'" alt="thumbnail" />
+                <h4 class="post-title">{{post.title}}</h4>
+              </div>
+              <div class="post-footer">
+                <a href="">Dismiss post</a>
+                <h4>{{post.comments}} comments</h4>
+              </div>
+            </div>
+          </div>
+          <div class="posts-block--footer">
+            <a href="">Dismiss All</a>
+          </div>
+        </div>
       </div>
       <div class="right-block">
         hosd
@@ -17,51 +41,44 @@ import axios from 'axios';
 export default {
   name: 'HelloWorld',
   elementsToShow: '',
-  post(data) {
-    let ObjPost = new Object();
-    ObjPost.title = data.title;
-    ObjPost.author = data.author;
-    ObjPost.entryDate = () => {
-      const createdDate = data.created;
+  methods:{
+    getPosts() {
+      let afterParam = '';
+      const itemsToShow = [];
+      axios
+        //.get("https://api.reddit.com/top.json?sort=top&limit=50")
+        .get(`https://api.reddit.com/top.json?sort=top&limit=50${afterParam ? '&after=' + afterParam: ''}`)
+        .then(res => {
+          const items = res.data.data.children;
+          afterParam = res.data.after;
+          items.forEach(element => {
+            console.log('ELEMENT', element.data.thumbnail);
+            const objPost = {
+              title: element.data.title,
+              author: element.data.author,
+              entryDate: () => {
+                const diff = Math.floor(new Date().getTime()/1000) - element.data.created_utc;
+                const formatted = Math.round(diff / 3600);
+                return `${formatted} hours ago`;
+              },
+              thumbnail: element.data.thumbnail,
+              comments: element.data.num_comments,
+              unread: true
+            }
+            console.log(objPost.entryDate())
+            itemsToShow.push(objPost);
+          });
+        })
+      return itemsToShow;
     }
-    ObjPost.thumb = data.thumbnail;
-    ObjPost.comments = data.comments;
-    ObjPost.unread = true;
-    return ObjPost;
   },
-  created: () => {
-    let afterParam = '';
-    const itemsToShow = [];
-    axios
-      //.get("https://api.reddit.com/top.json?sort=top&limit=50")
-      .get(`https://api.reddit.com/top.json?sort=top&limit=50${afterParam ? '&after=' + afterParam: ''}`)
-      .then(res => {
-        const items = res.data.data.children;
-        afterParam = res.data.after;
-        items.forEach(element => {
-          console.log(element)
-          const objPost = {
-            title: element.data.title,
-            author: element.data.author,
-            entryDate: () => {
-              const diff = Math.floor(new Date().getTime()/1000) - element.data.created_utc;
-              const formatted = Math.round(diff / 3600);
-              return `${formatted} hours ago`;
-            },
-            thumbnail: element.data.thumbnail,
-            comments: element.data.num_comments,
-            unread: true
-          }
-          console.log(objPost.entryDate())
-          itemsToShow.push(objPost);
-        });
-      })
-    this.elementsToShow = itemsToShow;
-    console.log(this.elementsToShow)
+  mounted () {
+    this.posts = this.getPosts();
   },
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
+      posts: []
     };
   },
 };
@@ -83,15 +100,66 @@ li {
 a {
   color: #42b983;
 }
-.container {
-  margin: 0 auto;
-  max-width: 1200px;
+.posts-block {
+  display: flex;
+  flex-flow: column;
 }
-.wrapper {
+.posts-block--header,
+.posts-block--footer {
+  align-items: center;
+  background: var(--color-dark-gray);
+  color: var(--color-white);
+  display: flex;
+  height: 50px;
+  justify-content: center;
+}
+.posts-block--content .post{
+  border-bottom: 1px solid var(--color-gray);
+}
+.post {
+  position: relative;
+}
+.post-author {
+  align-items: center;
   display: flex;
   flex-flow: row;
+  justify-content: flex-end;
 }
-.left-block, .right-block {
-  flex: 1;
+.post-author h5 {
+  font-style: italic;
+}
+.post-author h6 {
+  font-size: .7rem;
+  font-style: italic;
+  margin-left: 10px;
+}
+.post-content {
+  display: flex;
+  flex-flow: row;
+  margin-bottom: 20px;
+}
+.post-content .post-title {
+  margin: 0;
+  padding-left: 30px;
+  text-align: left;
+  width: 100%;
+}
+.post-content img {
+  height: 100%;
+  width: 100%;
+}
+.post-footer {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+}
+.unseen {
+  background: blue;
+  border-radius: 50%;
+  height: 15px;
+  left: 10px;
+  position: absolute;
+  top: 10px;
+  width: 15px;
 }
 </style>
